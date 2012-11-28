21a0cef3857d9d9fdd484321eb88963a9d02d22b
Loz Calver <lozcalver@bigfork.co.uk>
2012-11-28 15:10:16 +0000

    Add test & callback functionality

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Simply clone or download this repository and put it in a folder called 'yepnopes
 ##Examples:##
 
 ###Add files:###
-The simplest way to add a file, whether it's Javascript or CSS, is to use the `add_file()` function in your `Page_Controller`:
+The simplest way to add a file, or multiple files, whether they're Javascript or CSS, is to use the `add_files()` function in your `Page_Controller`:
 
 ```php
 class Page_Controller extends ContentController {
@@ -24,15 +24,23 @@ class Page_Controller extends ContentController {
 	public function init() {
 		parent::init();
 		
-		Yepnope::add_file('themes/yourtheme/javascript/yourjavascript.js');
-		Yepnope::add_file('themes/yourtheme/css/extrastyles.css');
+		/* Add a single file */
+		Yepnope::add_files('themes/yourtheme/js/yourjavascript.js');
+
+		/* Add an array of files */
+		Yepnope::add_files(
+			array(
+				'themes/yourtheme/css/adminstyles.css',
+				'themes/yourtheme/css/extrauserstyles.css'
+			)
+		);
 	}
 
 }
 ```
 
-###Remove files:###
-You can also remove a file from certain page types by using the `clear()` function. For this example, let's assume that you want to load the Javascript file from the above example on every page _except_ the home page:
+###Callbacks:###
+You can specify a _callback_ function (called for each file that's loaded) and/or a _complete_ function (called after all files have been loaded) by passing second and third arguments to the `add_files()` function:
 
 ```php
 class HomePage_Controller extends Page_Controller {
@@ -40,7 +48,61 @@ class HomePage_Controller extends Page_Controller {
 	public function init() {
 		parent::init();
 	
-		Yepnope::clear('themes/yourtheme/javascript/yourjavascript.js');
+		/* Will log 'loaded file' to console twice, once for each resource loaded */
+		Yepnope::add_files(
+			array(
+				'themes/yourtheme/js/filea.js',
+				'themes/yourtheme/js/fileb.js'
+			),
+			"console.log('loaded file')"
+		);
+
+		/* Will log 'loaded file' to console once, after both resources have been loaded */
+		Yepnope::add_files(
+			array(
+				'themes/yourtheme/js/filea.js',
+				'themes/yourtheme/js/fileb.js'
+			),
+			null,
+			"console.log('loaded file')"
+		);
+	}
+
+}
+```
+
+###Tests:###
+You can make use of yepnope's _test_ functionality with the `add_test()` function. Arguments must be passed in the following order:
+
+```php
+Yepnope::add_test($test, $yep=null, $nope=null, $load=null, $callback=null, $complete=null)
+```
+
+* `$test` - The test to be evaluated
+* `$yep` - The file (or array of files) to be loaded if the test returns _true_
+* `$nope` - The file (or array of files) to be loaded if the test returns _false_
+* `$callback` - The function to be called after each resource is loaded
+* `$complete` - The function to be called once after all resources are loaded
+
+Example tests:
+
+```php
+class HomePage_Controller extends Page_Controller {
+
+	public function init() {
+		parent::init();
+	
+		Yepnope::add_test(
+			'Modernizr.geolocation',			// Test if geolocation functionality exists
+			'regular-styles.css',				// If it does (test returns true), load regular styles
+			array(								// If it doesn't (test returns false), load extra files
+				'modified-styles.css',
+				'geolocation-polyfill.js'
+			),
+			'standardfunctions.js',				// Load these files regardless of test outcome
+			"console.log('loaded file')",		// Call this function upon loading each resource
+			"console.log('all files loaded')"	// Call this function when all files have been loaded
+		);
 	}
 
 }
@@ -56,16 +118,31 @@ class Page_Controller extends ContentController {
 		parent::init();
 		
 		Yepnope::set_yepnope(false) // Removes built-in requirement for yepnope.js script
-		Yepnope::set_yepnope('themes/yourtheme/javascript/modernizr.min.js'); // Sets requirement to use a custom script
+		Yepnope::set_yepnope('themes/yourtheme/js/modernizr.min.js'); // Sets requirement to use a custom script
 	}
 
 }
 ```
 
+###Tips:###
+If your _tests_, _callback_ functions or _complete_ functions are quite long, then putting them in a PHP file is ugly and hard to maintain. One alternative is to store the raw javascript in a template (be sure to do a _?flush=1_) and load it using the following method:
+
+```php
+class Page_Controller extends ContentController {
+
+	public function init() {
+		parent::init();
+		
+		Yepnope::add_files('themes/simple/javascript/somescript.js', $this->renderWith('MyCallback'));
+	}
+
+}
+```
+
+MyCallback.ss would then contain your raw Javascript (not wrapped in any HTML tags or anything).
+
 ##TODO##
 
-* Add functionality for yepnope's tests
-* Add support for callbacks on all files, and individual files
 * Add support for prefixes (!css, !timeout etc)
 * General cleaning, tidying and refactoring of code
 * More testing
--- code/Yepnope.php
+++ code/Yepnope.php
@@ -33,33 +33,41 @@ class Yepnope extends Requirements {
 	}
 
 	/**
-	 * Add a file to be output by yepnope
+	 * Add files to be output by yepnope
 	 *
-	 * @param string $file
+	 * @param string|array $files
+	 * @param string $callback A function called for each resource loaded
+	 * @param string $complete A function called once when all resources have been loaded
 	 */
-	public static function add_file($file) {
-		self::backend()->add_file($file);
+	public static function add_files($files, $callback=null, $complete=null) {
+		self::backend()->add_files($files, $callback, $complete);
 	}
 
 	/**
-	 * Clear either a single or all requirements.
-	 * Caution: Clearing single rules works only with customCSS and customScript if you specified a {@uniquenessID}. 
-	 * 
-	 * See {@link Requirements_Backend::clear()}
-	 * 
-	 * @param $file String
+	 * Add a yepnope test
+	 *
+	 * @param string $test The test to be run
+	 * @param string|array $yep File(s) to be loaded if the test passes
+	 * @param string|array $nope File(s) to be loaded if the test fails
+	 * @param string|array $load File(s) to be loaded regardless of the test outcome
+	 * @param string $callback A function called for each resource loaded
+	 * @param string $complete A function called once when all resources have been loaded 
 	 */
-	public static function clear($fileOrID = null) {
-		self::backend()->clear($fileOrID);
+	public static function add_test($test, $yep=null, $nope=null, $load=null, $callback=null, $complete=null) {
+		self::backend()->add_test($test, $yep, $nope, $load, $callback, $complete);
 	}
 
 }
 
 class Yepnope_Backend extends Requirements_Backend {
 
-	protected $yepnopeFiles = array();
+	//protected $yepnopeFiles = array();
+
+	protected $yepnopeTests = array();
 
 	protected $yepnopeScript = 'yepnopesilverstripe/javascript/yepnope.1.5.4-min.js';
+
+	public $customScriptID = null;
 
 	public function set_yepnope($file) {
 		$this->yepnopeScript = $file;
@@ -69,48 +77,81 @@ class Yepnope_Backend extends Requirements_Backend {
 		return $this->yepnopeScript;
 	}
 
-	public function add_file($file) {
-		$this->yepnopeFiles[$file] = true;
-
+	public function add_files($files, $callback=null, $complete=null) {
+		if(is_string($files)) $files = array($files);
+		$yepnopeTest = array(
+			'test' => null,
+			'yep' => null,
+			'nope' => null,
+			'load' => $files,
+			'callback' => $callback,
+			'complete' => $complete
+		);
+		
+		$this->yepnopeTests[] = $yepnopeTest;
 		$this->evalYepnope();
 	}
 
-	/**
-	 * Clear either a single or all requirements.
-	 * Caution: Clearing single rules works only with customCSS and customScript if you specified a {@uniquenessID}. 
-	 * 
-	 * @param string $fileOrID
-	 */
-	public function clear($fileOrID = null) {
-		$arrayList = array('javascript','css', 'customScript', 'customCSS', 'customHeadTags', 'yepnopeFiles');
-		if($fileOrID) {
-			foreach($arrayList as $type) {
-				if(isset($this->{$type}[$fileOrID])) {
-					$this->disabled[$type][$fileOrID] = $this->{$type}[$fileOrID];
-					unset($this->{$type}[$fileOrID]);
-				}
-			}
-		} else {
-			foreach ($arrayList as $type) {
-				$this->disabled[$type] = $this->{$type};
-				$this->{$type} = array();
-			}
+	public function add_test($test, $yep=null, $nope=null, $load=null, $callback=null, $complete=null) {
+		if ( ! $yep && ! $nope) {
+			user_error(
+				"Yepnope::add_test(): 
+				You need to specify a 'yep' or a 'nope' for your test.", 
+				E_USER_ERROR
+			);
 		}
+
+		if(is_string($yep)) $yep = array($yep);
+		if(is_string($nope)) $nope = array($nope);
+		if(is_string($load)) $nope = array($load);
+
+		$yepnopeTest = compact("test", "yep", "nope", "load", "callback", "complete");
+		
+		$this->yepnopeTests[] = $yepnopeTest;
 		$this->evalYepnope();
 	}
 
 	public function evalYepnope() {
 		if ($yepnope = $this->get_yepnope()) Requirements::javascript($yepnope);
+		if ($this->customScriptID) $this->clear($this->customScriptID);
 
-		$allYepnopeFiles = array();
+		$str = "yepnope([{\n";	
+		$allYepnopeTests = array();
 
-		foreach ($this->yepnopeFiles as $file => $val) {
-			$path = $this->path_for_file($file);
-			$allYepnopeFiles[] = $path;
+		foreach ($this->yepnopeTests as $test) {
+			$testStr = '';
+			if ($test['test']) {
+				$testStr .= "test: " . $test['test'];
+				if (isset($test['yep'])) {
+					$testStr .= ",\nyep: ['";
+					$testStr .= implode("', '", $test['yep']);
+					$testStr .= "']";
+				}
+				if (isset($test['nope'])) {
+					$testStr .= ",\nnope: ['";
+					$testStr .= implode("', '", $test['nope']);
+					$testStr .= "']";
+				}
+				$testStr .= ",\n";
+			}
+			if (isset($test['load'])) {
+				$testStr .= "load: ['";
+				$testStr .= implode("', '", $test['load']);
+				$testStr .= "']";
+			}
+			if (isset($test['callback'])) {
+				$testStr .= ",\ncallback: " . $test['callback'];
+			}
+			if (isset($test['complete'])) {
+				$testStr .= ",\ncomplete: " . $test['complete'];
+			}
+			$allYepnopeTests[] = $testStr .= "\n";
 		}
 
-		$str = "yepnope(['" . implode("','", $allYepnopeFiles) . "'])";
-		Requirements::customScript($str, time());
+		$str .= implode("}, {\n", $allYepnopeTests) . "}]);";
+		
+		$this->customScriptID = time();
+		Requirements::customScript($str, $this->customScriptID);
 	}
 
 }
