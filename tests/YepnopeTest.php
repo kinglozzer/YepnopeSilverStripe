bdd464520f0903f7def9a5b19632b99e21eb08ec
Loz Calver <kinglozzer@gmail.com>
2014-01-24 21:34:26 +0000

    Unit testing / Travis setup

---
+++ tests/YepnopeTest.php
@@ -0,0 +1,131 @@
+<?php
+
+class YepnopeTest extends SapphireTest {
+
+	public function setUp() {
+		Requirements::clear();
+
+		// Reset Yepnope::$backend to null for each test
+		$yep = new Yepnope();
+		$reflection = new ReflectionClass('Yepnope');
+		$backendProp = $reflection->getProperty('backend');
+		$backendProp->setAccessible(true);
+		$backendProp->setValue($yep, null);
+	}
+	
+	public function testSetters() {
+		$backend = Yepnope::backend();
+		Yepnope::set_yepnope('foo.js');
+		Yepnope::set_automatically_evaluate(false);
+		Yepnope::set_timeout(10000);
+
+
+		$this->assertInstanceOf('Yepnope_Backend', $backend);
+		$this->assertEquals('foo.js', $backend->get_yepnope());
+		$this->assertFalse($backend->get_automatically_evaluate());
+		$this->assertEquals(10000, $backend->get_timeout());
+	}
+
+	public function testAddFiles() {
+		$files = array('1.js', '2.js');
+		$callback = 'function() { callback(); }';
+		$complete = 'function() { complete(); }';
+		Yepnope::add_files($files, $callback, $complete);
+		Yepnope::eval_yepnope();
+
+		$scripts = Requirements::get_custom_scripts();
+		
+		$expects = <<<JS
+yepnope([{
+	load: ['1.js', '2.js'],
+	callback: function() { callback(); },
+	complete: function() { complete(); }
+}]);
+JS;
+
+		$this->assertContains($expects, $scripts);
+	}
+
+	public function testClearTest() {
+		$files1 = array('1.js', '2.js');
+		$files2 = array('3.js', '4.js');
+		Yepnope::add_files($files1, null, null, 'ClearMe');
+		Yepnope::add_files($files2, null, null, 'ButNotMe');
+		Yepnope::clear_test('ClearMe');
+		Yepnope::eval_yepnope();
+
+		$scripts = Requirements::get_custom_scripts();
+		
+		$this->assertContains("load: ['3.js', '4.js']", $scripts);
+		$this->assertNotContains("load: ['1.js', '2.js']", $scripts);
+	}
+
+	public function testAddTest() {
+		$test = "window.JSON";
+		$yep = array('yep.js');
+		$nope = array('nope.js');
+		$load = array('always-load.js');
+		$callback = 'function() { callback(); }';
+		$complete = 'function() { complete(); }';
+		Yepnope::add_test($test, $yep, $nope, $load, $callback, $complete);
+		Yepnope::eval_yepnope();
+
+		$scripts = Requirements::get_custom_scripts();
+		
+		$expects = <<<JS
+yepnope([{
+	test: window.JSON,
+	yep: ['yep.js'],
+	nope: ['nope.js'],
+	load: ['always-load.js'],
+	callback: function() { callback(); },
+	complete: function() { complete(); }
+}]);
+JS;
+		
+		$this->assertContains($expects, $scripts);
+
+		$test2 = "Modernizr.geolocation";
+		$yep2 = array('hasGeo.js');
+		$nope2 = array('geoPolyfill.js');
+		$load2 = array('geo-app.js');
+		$callback2 = 'function() { geoLoaded(); }';
+		$complete2 = 'function() { geoComplete(); }';
+		Yepnope::add_test($test2, $yep2, $nope2, $load2, $callback2, $complete2);
+		Yepnope::eval_yepnope();
+
+		$scripts = Requirements::get_custom_scripts();
+
+		$expects = <<<JS
+yepnope([{
+	test: window.JSON,
+	yep: ['yep.js'],
+	nope: ['nope.js'],
+	load: ['always-load.js'],
+	callback: function() { callback(); },
+	complete: function() { complete(); }
+}, {
+	test: Modernizr.geolocation,
+	yep: ['hasGeo.js'],
+	nope: ['geoPolyfill.js'],
+	load: ['geo-app.js'],
+	callback: function() { geoLoaded(); },
+	complete: function() { geoComplete(); }
+}]);
+JS;
+
+		$this->assertContains($expects, $scripts);
+	}
+
+	public function testGetTest() {
+		$files = array('1.js', '2.js');
+		$callback = 'function() { callback(); }';
+		$complete = 'function() { complete(); }';
+		Yepnope::add_files($files, $callback, $complete, 'test-id');
+
+		$test = Yepnope::get_test('test-id');
+
+		$this->assertInstanceOf('YepnopeTestObject', $test);
+	}
+
+}
---
+++ tests/YepnopeTestObjectTest.php
@@ -0,0 +1,47 @@
+<?php
+
+class YepnopeTestObjectTest extends SapphireTest {
+
+	protected $test;
+
+	public function setUp() {
+		$this->test = new YepnopeTestObject(
+			'id-1',
+			'test-1',
+			'yep-1',
+			'nope-1',
+			'load-1',
+			'callback-1',
+			'complete-1'
+		);
+	}
+	
+	public function testGetters() {
+		$this->assertEquals('id-1', $this->test->getID());
+		$this->assertEquals('test-1', $this->test->getTest());
+		$this->assertEquals('yep-1', $this->test->getYep());
+		$this->assertEquals('nope-1', $this->test->getNope());
+		$this->assertEquals('load-1', $this->test->getLoad());
+		$this->assertEquals('callback-1', $this->test->getCallback());
+		$this->assertEquals('complete-1', $this->test->getComplete());
+	}
+
+	public function testSetters() {
+		$this->test->setID('id-2');
+		$this->test->setTest('test-2');
+		$this->test->setYep('yep-2');
+		$this->test->setNope('nope-2');
+		$this->test->setLoad('load-2');
+		$this->test->setCallback('callback-2');
+		$this->test->setComplete('complete-2');
+
+		$this->assertEquals('id-2', $this->test->getID());
+		$this->assertEquals('test-2', $this->test->getTest());
+		$this->assertEquals('yep-2', $this->test->getYep());
+		$this->assertEquals('nope-2', $this->test->getNope());
+		$this->assertEquals('load-2', $this->test->getLoad());
+		$this->assertEquals('callback-2', $this->test->getCallback());
+		$this->assertEquals('complete-2', $this->test->getComplete());
+	}
+
+}
